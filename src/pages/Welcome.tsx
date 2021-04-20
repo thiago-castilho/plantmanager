390ee0a2e3f448ac6c1c890d1e03034d64a01b82
Thiago Castilho <tcosta1996@hotmail.com>
2021-04-20 11:05:45 -0300

    primeira tela finalizada

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,36 +1,51 @@
-import React, { useState } from "react";
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions
+} from "react-native";
+import { Feather } from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png';
-import colors from '../../src/styles/colors';
-import { Button } from "../components/Button";
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
-  const [visible, setVisible] = useState(false);
-
-  function handleVisibility() {
-    setVisible(true)
-  }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma fácil
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+        suas plantas de {'\n'}
+        forma fácil
       </Text>
 
-      {
-        visible &&
-        <Image source={wateringImg} style={styles.image} />
-      }
+        <Image
+          source={wateringImg}
+          style={styles.image}
+          resizeMode='contain'
+        />
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. {'\n'}
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. {'\n'}
         Nós cuidamos de lembrar você sempre que precisar.
       </Text>
-
-      <Button title=">" onPress={handleVisibility} />
-      <Text />
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+        >
+          {/* <Text style={styles.buttonText}> */}
+          <Feather
+            name="chevron-right"
+            style={styles.buttonIcon}
+          />
+          {/* </Text> */}
+        </TouchableOpacity>
+        <Text />
+      </View>
     </View>
 
   );
@@ -38,25 +53,44 @@ export function Welcome() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  wrapper: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20,
+    paddingTop: 80
   },
   title: {
-    paddingTop: 80,
-    fontSize: 32,
-    fontWeight: 'bold',
+    // paddingTop: 80,
+    fontSize: 28,
     textAlign: 'center',
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 34
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
   },
   image: {
-    width: 292,
-    height: 284
+    height: Dimensions.get('window').width * 0.7
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 32,
   }
 });
