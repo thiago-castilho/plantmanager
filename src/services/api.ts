99614f744d35c01ffc051d8c33fad498eff9fdfb
Thiago Castilho <tcosta1996@hotmail.com>
2021-04-23 17:57:26 -0300

    add my-json-server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.15.7:3333",
+  baseURL: "https://my-json-server.typicode.com/thiago-castilho/plantmanager",
 });
 
 export default api;
